6137d1255621c91bf8b31e613a7461ce7fcbb7c0
ricardo benitez <ricardocdelu@gmail.com>
2022-07-26 20:25:37 -0300

    objetos

--- JS/script.js
+++ JS/script.js
@@ -1,58 +1,60 @@
 
-let nombreProducto1 = "cerveza ipa"
-let precioProducto1 = 400
-let stockProducto1 = 20
+function producto(nombre, precio, stock){
+  this.nombre = nombre
+  this.precio = precio
+  this.stock = stock
+  this.sumarIva = function (){
+    this.precio = this.precio * 1.21
+}
+}
 
-let nombreProducto2 = "cerveza doble ipa"
-let precioProducto2 = 450
-let stockProducto2 = 20
+const producto1 = new producto("cerveza ipa", 400, 20)
+const producto2 = new producto("cerveza doble ipa", 450, 20)
+const producto3 = new producto("cerveza neipa", 400, 20)
+const producto4 = new producto("cerveza apa", 350, 20)
+const producto5 = new producto("cerveza white ipa", 350, 20)
+const producto6 = new producto("cerveza black lager", 450, 20)
 
-let nombreProducto3 = "cerveza neipa"
-let precioProducto3 = 400
-let stockProducto3 = 20
-
-let nombreProducto4 = "cerveza apa"
-let precioProducto4 = 350
-let stockProducto4 = 20
-
-
-let cantidadCompras = prompt("ingrese la cantidad de productos distintos que quiere comprar: \n- cerveza ipa\n- cerveza doble ipa\n- cerveza neipa  \n- cerveza apa");
+let listaProductos = [producto1, producto2, producto3, producto4, producto5, producto6]
+let nombresProductos = listaProductos.map((producto) => producto.nombre)
+let cantidadCompras = prompt("Ingrese la cantidad de productos distintos que quiere comprar:\n " + nombresProductos.join("\n "))
 let precioTotal = 0;
 
 function calculoPrecio(cantidad, precio){
     precioTotal += cantidad * precio
 }
 
-function calculoStock(cantidad, stock, precio){
-    if(stock >= cantidad){
-        calculoPrecio(cantidad, precio)
-        alert("El precio total es de: $" + (cantidad * precio))
+function calculoStock(cantidad, producto){
+    if(producto.stock >= cantidad){
+        calculoPrecio(cantidad, producto.precio)
+        alert("El precio total es de: $" + (cantidad * producto.precio))
     }
     else{
         alert("No disponemos de esa cantidad en stock. Nuestro stock actual es de: " + stock + " unidades")
     }
 }
-
-function sumaIva(precio){
-    return precio * 1.21
-}
-
 for(let i = 0; i < cantidadCompras; i++){
 
-    let compra1 = prompt("ingrese el nombre del producto que quiere comprar:\n- cerveza ipa\n- cerveza doble ipa\n- cerveza neipa  \n- cerveza apa");
+    let compra1 = prompt("Ingrese el nombre del producto que quiere comprar:\n " + nombresProductos.join("\n ")).toLowerCase()
     let cantidad1 = prompt("ingrese la cantidad del producto que quiere comprar:");
 
-    if(compra1 == "cerveza ipa"){
-        calculoStock(cantidad1, stockProducto1, precioProducto1)
+    if(compra1 == producto1.nombre.toLowerCase()){
+        calculoStock(cantidad1, producto1)
     }
-    else if(compra1 == "cerveza doble ipa"){
-        calculoStock(cantidad1, stockProducto2, precioProducto2)
+    else if(compra1 == producto2.nombre.toLowerCase()){
+        calculoStock(cantidad1, producto2)
     }
-    else if(compra1 == "cerveza neipa"){
-        calculoStock(cantidad1, stockProducto3, precioProducto3)
+    else if(compra1 == producto3.nombre.toLowerCase()){
+        calculoStock(cantidad1, producto3)
     }
-    else if(compra1 == "cerveza apa"){
-        calculoStock(cantidad1, stockProducto4, precioProducto4)
+    else if(compra1 == producto4.nombre.toLowerCase()){
+        calculoStock(cantidad1, producto4)
+    }
+    else if(compra1 == producto5.nombre.toLowerCase()){
+        calculoStock(cantidad1, producto5)
+    }
+    else if(compra1 == producto6.nombre.toLowerCase()){
+        calculoStock(cantidad1, producto6)
     }
     else{
         alert("No tenemos ese producto")
@@ -60,19 +62,19 @@ for(let i = 0; i < cantidadCompras; i++){
 }
 
 switch(true){
-    case precioTotal < 1500 || precioTotal > 1000:
+    case precioTotal < 2000 || precioTotal > 1000:
         precioTotal = precioTotal * 0.95
         alert("Recibiste un descuento del 5% por tu compra")
         break;
-    case precioTotal < 3000:
+    case precioTotal < 5000:
         precioTotal = precioTotal * 0.90
         alert("Recibiste un descuento del 10% por tu compra")
         break;
-    case precioTotal < 4000:
+    case precioTotal < 10000:
         precioTotal = precioTotal * 0.80
         alert("Recibiste un descuento del 20% por tu compra")
         break;
-    case precioTotal > 6000:
+    case precioTotal > 10000:
         alert("Recibiste un descuento del 40% por tu compra")
         precioTotal = precioTotal * 0.60
         break;
@@ -81,8 +83,6 @@ switch(true){
         alert("No ingresaste un precio en numeros");
         break;
 }
-
-
 
 let precioToTalConImpuestos = sumaIva(precioTotal)
 
