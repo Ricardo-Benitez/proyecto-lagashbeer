c2797d6d045a3b23a9df6c298b98b2ed4d993334
ricardo benitez <ricardocdelu@gmail.com>
2022-08-04 01:45:44 -0300

    formulario event

---
+++ JS/script2.js
@@ -0,0 +1,16 @@
+
+let Formulario = document.getElementById("formulario");
+
+Formulario.addEventListener("submit", validateForm);
+
+function validateForm(e){
+
+e.preventDefault();
+
+let form = e.target
+
+console.log(form[0].value)
+console.log(form[1].value)
+
+console.log("Formulario Enviado"); 
+}   
